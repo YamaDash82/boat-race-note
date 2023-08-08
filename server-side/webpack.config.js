31f4fea96d5bc397aadf22720ea640acaf525f97
yamadash5785 <yamachang_never_die.19820805@ezweb.ne.jp>
2023-08-08 22:07:32 +0000

    NestJSの成果物を単一ファイル化できるようにwebpack.config.jsを修正。

--- server-side/webpack.config.js
+++ server-side/webpack.config.js
@@ -8,7 +8,17 @@ module.exports = {
         'commonjs2 @nestjs/websockets/socket-module',
       '@nestjs/microservices/microservices-module':
         'commonjs2 @nestjs/microservices/microservices-module',
-      '@fastify/static': 'commonjs2 @fastify/static'
+      '@fastify/static': 'commonjs2 @fastify/static',
+      '@as-integrations/fastify':
+        'commonjs2 @as-integrations/fastify',
+      '@apollo/subgraph': 'commonjs2 @apollo/subgraph', 
+      '@apollo/gateway': 'commonjs2 @apollo/gateway', 
+      '@apollo/subgraph/package.json':
+        'commonjs2 @apollo/subgraph/package.json', 
+      '@apollo/subgraph/dist/directives':
+        'commonjs2 @apollo/subgraph/dist/directives',
+      'ts-morph': 'commonjs2 ts-morph', 
+      'class-transformer/storage': 'commonjs2 class-transformer/storage', 
     },
   ],
   optimization: {
