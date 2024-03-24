d0df48b488215c79b8a84397997e2829afb02a61
yamadash5785 <yamachang_never_die.19820805@ezweb.ne.jp>
2024-03-24 14:07:23 +0000

    展開予想入力 ・サイドメニューバー仮設置 ・スリット描画仮実装

--- front-side/projects/main/src/app/prediction/deployment-prediction/deployment-prediction.component.ts
+++ front-side/projects/main/src/app/prediction/deployment-prediction/deployment-prediction.component.ts
@@ -5,6 +5,7 @@ import { FormControl, NonNullableFormBuilder } from '@angular/forms';
 import { PaginatorComponent } from '../../general/paginator/paginator.component';
 import { BoatColor, BoatColors } from 'projects/main/src/app/common/utilities';
 import { Subject } from 'rxjs';
+import { StartingFormation } from 'projects/main/src/generated/graphql';
 
 @Component({
   selector: 'app-deployment-prediction',
@@ -24,13 +25,55 @@ import { Subject } from 'rxjs';
           (moved)="pageMoved($event)"
           (beforeMove)="saveCurrentPrediction()"
         ></app-paginator>
+        <button 
+          (click)="buttonCotainerVisilbe=!buttonCotainerVisilbe"
+          class="ml-auto"
+          mat-icon-button
+        >
+          <mat-icon>menu</mat-icon>
+        </button>
       </div>
       <!--内容-->
-      <div #parentDeploymentPredictionCanvas class="grow bg-blue-400">
+      <div #parentDeploymentPredictionCanvas class="grow bg-blue-400 relative"
+        (click)="deploymentPredictionCanvasClick()"
+      >
         <canvas 
           id="deploymentPrediction" 
           #deploymentPredictionCanvas
         ></canvas>
+        <div 
+          class="absolute top-0 bg-red-500 left-[100%] translate-x-[-100%] h-full w-24"
+          *ngIf="buttonCotainerVisilbe"
+          >
+            <!--スリット描画-->
+            <div>
+              <button type="button"
+                mat-flat-button
+                (click)="drawStartingBoats(0)"
+              >
+                ST描画
+              </button>
+            </div>
+            <!--配置待ちボート選択ボタン-->
+            <div *ngFor="let boatButton of boatButtons">
+              <button type="button" 
+                mat-flat-button
+                [color]="waitPlacementBoatNo===boatButton.boatNo ? 'primary' : ''"
+                (click)="setWaitPlacementBoat(boatButton.boatNo, $event)"
+              >
+                {{boatButton.caption}}
+              </button>
+            </div>
+            <!--ボート削除ボタン-->
+            <div>
+              <button type="button"
+                mat-flat-button
+                (click)="removeBoat($event)"
+              >
+                削除
+              </button>
+            </div>
+          </div>
       </div>
     </div>
   `,
@@ -46,6 +89,20 @@ export class DeploymentPredictionComponent implements OnInit, AfterViewChecked, 
   @ViewChild('deploymentPredictionCanvas') deploymentPredictionCanvasEmt!: ElementRef;
   //private canvas!: fabric.Canvas;
   private deploymentPredictionCanvas!: DeploymentPredictionCanvas;
+  //キャンバスのサイズ
+  private canvasSize: { height: number, width: number } = { height: 0, width: 0 };
+  //ボートボタンコンテナー可視
+  buttonCotainerVisilbe = true;
+  boatButtons: { boatNo: number, caption: string }[] = [
+    { boatNo: 1, caption: "1号艇" }, 
+    { boatNo: 2, caption: "2号艇" }, 
+    { boatNo: 3, caption: "3号艇" }, 
+    { boatNo: 4, caption: "4号艇" }, 
+    { boatNo: 5, caption: "5号艇" }, 
+    { boatNo: 6, caption: "6号艇" }, 
+  ];
+  //配置待ち艇番
+  waitPlacementBoatNo: number | null = null;
 
   //サイズ変更検知用Subject
   private sizeChanged = new Subject<{ height: number, width: number}>();
@@ -68,8 +125,12 @@ export class DeploymentPredictionComponent implements OnInit, AfterViewChecked, 
   ngAfterViewInit(): void {
     //サイズ変更検知時の処理を登録する。展開予想キャンバスのサイズを設定する。
     this.sizeChanged$.subscribe(size => {
+        //キャンバスのサイズを保持する。
+        this.canvasSize = size;
+        //入力中の展開予想があればそれを再描画する。
         this.zone.runOutsideAngular(() => {
-          this.deploymentPredictionCanvas.drawInitialRacingPool(size.width, size.height);
+          //入力中の展開予想があれば、それを再描画すべき個所
+          //this.deploymentPredictionCanvas.drawInitialRacingPool(size.width, size.height);
         });
     });
 
@@ -99,10 +160,6 @@ export class DeploymentPredictionComponent implements OnInit, AfterViewChecked, 
     this.fg.deploymentPredictions.push(new FormControl<string | null>(null));
     //ページ末尾に移動する。※追加した予想をカレントにする。
     this.paginator.moveLast();
-
-
-    //テスト
-    this.setWaitPlacementBoat(6);
   }
 
   /**
@@ -134,15 +191,50 @@ export class DeploymentPredictionComponent implements OnInit, AfterViewChecked, 
       this.deploymentPredictionCanvas.drawInitialRacingPool();
       */
     });
-    this.deploymentPredictionCanvas.drawInitialRacingPool();
+    this.deploymentPredictionCanvas.drawInitialRacingPool(this.canvasSize.width, this.canvasSize.height);
   }
 
   /**
    * 配置待ちボート設定処理
    * @param boatNo  艇番
    */
-  setWaitPlacementBoat(boatNo: number) {
+  setWaitPlacementBoat(boatNo: number, event: MouseEvent) {
+    //イベントの伝搬を抑制する。
+    event.stopImmediatePropagation();
+
+    //配置待ち艇番をセットする
+    this.waitPlacementBoatNo = boatNo;
     this.deploymentPredictionCanvas.setWaitPlacementBoat(boatNo);
+  }
+
+  /**
+   * スタート艇描画
+   * @param approachPredictionIndex 
+   */
+  drawStartingBoats(approachPredictionIndex: number) {
+    this.deploymentPredictionCanvas.drawStartigBoats(this.fg.approachPredictions.controls[0].toDto());
+  }
+
+  /**
+   * ボート削除処理
+   * キャンバス中で選択中のオブジェクト(ボートを表したトライアングル)があれば、それをキャンバスから除外する。
+   */
+  removeBoat(event: MouseEvent) {
+    //イベントの伝搬を抑制する。
+    event.stopImmediatePropagation();
+
+    const activeBoat = this.deploymentPredictionCanvas.getActiveObject()
+    if (activeBoat) {
+      this.deploymentPredictionCanvas.remove(activeBoat);
+    }
+  }
+
+  /**
+   * 展開予想キャンバスクリック時処理
+   */
+  deploymentPredictionCanvasClick() {
+    this.waitPlacementBoatNo = null;
+    console.log(`キャンバスクリック`);
   }
 
   ngOnDestroy(): void {
@@ -289,8 +381,59 @@ export class DeploymentPredictionCanvas extends fabric.Canvas {
     });
 
     this.add(centerLine, startLine, turnMark);
+  }  
+  
+  //進入体系描画処理
+  drawStartigBoats(approachPrediction: StartingFormation): void {
+    /*
+    approachPrediction.boats.forEach(boat => {
+
+    });
+    */
+    //ボートを配置する縦位置の基準
+    const verticalBase = Math.ceil(this.canvasHeight * DeploymentPredictionCanvas.centerLineLocationRatio[1]);
+    //コースごとに増す高さ
+    const verticalCoefficient = Math.ceil((this.canvasHeight - verticalBase) / 7);
+    //スタートライン
+    const startLine = Math.ceil(this.canvasWidth * DeploymentPredictionCanvas.startLineLocationRatio[0]);
+
+    //ボートのサイズについて、マウスダウンでボート配置するときと同じ値設定を別々の個所で行っているので後で処理をまとめる。
+    //ボートの高さ、キャンバスの高さから割合で導く。
+    const boatWidth = Math.floor(this.canvasHeight / 30);
+    //ボートの規格より 幅133.6cm、全長289.5cmから2.17を算出。
+    const boatHeight = Math.floor(boatWidth * 2.17);
+
+    //スタートタイミングからスタート位置を割り出す際の基準幅
+    //0.1秒は1.5艇身をもとに計算
+    //1艇身×6.67倍で1秒の幅を表す。これにスタートタイミングを乗ずることでスタート位置を表す。
+    const baseStartWidth = boatHeight * 6.67;
+
+    const startingBoatas = [
+      approachPrediction.course1, 
+      approachPrediction.course2, 
+      approachPrediction.course3, 
+      approachPrediction.course4, 
+      approachPrediction.course5, 
+      approachPrediction.course6, 
+    ];
+
+    startingBoatas.forEach(boat => {
+      //スタート位置
+      const startPosition = startLine - (baseStartWidth * (boat.st || 1));
+
+      const dBoat = new Boat(
+        boat.boat_no,
+        verticalBase + verticalCoefficient * boat.boat_no, //ボートを描画する縦位置
+        startPosition,
+        boatWidth,
+        boatHeight
+      );
+
+      this.add(dBoat);
+    });
   }
 }
+
 //ボート
 //ボートを表す三角形
 class Boat extends fabric.Triangle {
