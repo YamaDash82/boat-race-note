56c3621276b4e8e62199bd61d3a4d1f4e5e55c20
yamadshs5785 <yamadashs5785@gmail.com>
2024-05-26 01:19:52 +0000

    サーバーサイド レース予想抽出処理を実装。 ユーザーキー（必須）、抽出開始日、抽出終了日、開催上での抽出を実装。

--- server-side/src/race-predictions/race-predictions.resolver.ts
+++ server-side/src/race-predictions/race-predictions.resolver.ts
@@ -12,4 +12,18 @@ export class RacePredictionsResolver {
   async racePrediction(@Args('key') key: string) {
     return this.racePredictionsSvc.findOne(key);
   }
+
+  @Query(() => [RacePredictionModel], { nullable: true })
+  async racePradictions(
+    //ユーザーキー 必須
+    @Args('user_key') userKey: string,
+    //抽出開始日
+    @Args('date_from', { nullable: true }) dateFrom?: string, 
+    //抽出終了日
+    @Args('date_to', { nullable: true }) dateTo?: string, 
+    //開催場
+    @Args('race_place_cd', { nullable: true }) racePlaceCd?: number
+  ) {
+    return this.racePredictionsSvc.find(userKey, dateFrom, dateTo, racePlaceCd);
+  }
 }
