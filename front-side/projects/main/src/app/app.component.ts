1d21f7199258bef4dc24262fde4cb29391aad653
yamadshs5785 <yamadashs5785@gmail.com>
2024-06-11 13:30:22 +0000

    サービスワーカーを使用して、オートリロードを実装。

--- front-side/projects/main/src/app/app.component.ts
+++ front-side/projects/main/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth/auth.service';
 import { UsersModel } from '../generated/graphql';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   template: `
     <header class="h-[5vh] w-full bg-red-500 flex justify-between items-center text-4xl">
-      <div>{{title}}</div>
+      <div>ボートレース予想</div>
       <div>{{loginUser?.key}}</div>
     </header>
     <main class="h-[90vh] w-full bg-slate-300">
@@ -19,14 +20,20 @@ import { UsersModel } from '../generated/graphql';
   styles: []
 })
 export class AppComponent implements OnInit {
-  title = 'main';
   loginUser: UsersModel | null = null;
 
   constructor(
+    private swUpdate: SwUpdate, 
     private auth: AuthService, 
   ) { }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+    //新しいバージョンのリリースをチェックする。
+    if (await this.swUpdate.checkForUpdate()) { 
+      //新しいバージョンがあればリロードする。
+      document.location.reload();
+    }
+
     this.auth.loginStateChange$.subscribe(user => {
       this.loginUser = user;
     });
