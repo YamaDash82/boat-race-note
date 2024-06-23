3e007735f39464e88e7888bd23959fabdff30c83
yamadshs5785 <yamadashs5785@gmail.com>
2024-06-23 06:20:41 +0000

    改行位置を整えた。

--- front-side/projects/main/src/app/app.module.ts
+++ front-side/projects/main/src/app/app.module.ts
@@ -31,12 +31,13 @@ import { ServiceWorkerModule } from '@angular/service-worker';
       }
     }),
     AuthModule, 
-    PredictionModule, ServiceWorkerModule.register('ngsw-worker.js', {
-  enabled: !isDevMode(),
-  // Register the ServiceWorker as soon as the application is stable
-  // or after 30 seconds (whichever comes first).
-  registrationStrategy: 'registerWhenStable:30000'
-}), 
+    PredictionModule, 
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }), 
   ],
   providers: [
     AuthService, 
